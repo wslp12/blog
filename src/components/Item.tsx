9e3110c216f12774ae7fa1cabec9d474aaa565d3
jimmy <jimmy@collabee.co>
2022-09-01 13:56:44 +0900

    .

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function Item() {
   const { itemParam } = useParams();
 
-  useEffect(() => {
-    console.log('mount');
-
-    return () => console.log('unmount');
-  }, []);
   return <div>{itemParam}</div>;
 }
 
